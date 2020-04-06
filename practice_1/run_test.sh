31a064ac26f0234ace07d8b94f72aa92b5d96ea5
jinhao <425589305@qq.com>
2020-04-06 23:48:06 +0800

    practice Day1

---
+++ practice_1/run_test.sh
@@ -0,0 +1,14 @@
+## test1 
+<<'!'
+Note: Rise sample missing error when run as "snakemake -s Sankefile_test1 -np  mapped_reads/{happy_1}.bam".
+The out put files must be given when execute the Sankefile_test1. (因为没有指明输入文件，只能让snake从output回推)
+!
+snakemake -s Sankefile_test1 -np  mapped_reads/{happy_1,happy_2}.bam
+
+## test2
+<<'!'
+You can also assign the input file.
+!
+snakemake -s Sankefile_test2 -np
+
+## tese3
